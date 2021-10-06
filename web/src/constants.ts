29762b6a2b493358e67eeb6bf7ce9203a6b1c957
irere123 <irere2020@outlook.com>
2021-10-06 22:55:43 +0200

    initial added db conn conf stuff

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,4 +1,4 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 export const apiBaseUrl = !__prod__
   ? "http://localhost:4000"
-  : "https://next-neox.vercel.app";
+  : "https://api-neox-next.herokuapp.com";
