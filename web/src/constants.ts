1c40d88f0000a08a08a1fe8016bab1d72e804643
irere123 <irere2020@outlook.com>
2021-10-06 21:59:58 +0200

    initial added db conn conf stuff

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,1 +1,4 @@
-export const apiBaseUrl = "http://localhost:4000";
+export const __prod__ = process.env.NODE_ENV === "production";
+export const apiBaseUrl = !__prod__
+  ? "http://localhost:4000"
+  : "https://next-neox.vercel.app";
