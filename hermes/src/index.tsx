41f96afae299efca53d43b2bd45ae35416c17c3c
irere123 <irere2020@outlook.com>
2021-12-30 11:15:22 +0200

    feat: added community folder

--- hermes/src/index.tsx
+++ hermes/src/index.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  NormalizedCacheObject,
+} from "@apollo/client";
 
 import "./index.css";
 import App from "./app/Routes";
 
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+  credentials: "include",
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
