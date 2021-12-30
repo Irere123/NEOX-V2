c71c9db423075f818b4656287c184faaf2c73e92
irere123 <irere2020@outlook.com>
2021-12-30 02:59:13 +0200

    new: refactored folder structure and change names

--- server/friendsSys.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM users AS u JOIN friends AS f ON u.id = f.userId WHERE  f.friendId = 2
-UNION SELECT * FROM users as u join friends AS f ON p.userId  = f.friendId WHERE f.userId
--- server/src/resolvers/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Team } from "./teams/team";
-export { default as User } from "./user/user";
-export { default as Room } from "./rooms/rooms";
-export { default as Message } from "./messages/messages";
